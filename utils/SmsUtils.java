3e6f25f2af1415780a257c4f39bfadc026d97bf3
jason.zhao <jason.plus.zhao@gmail.com>
2020-11-30 16:15:56 +0800

    update

--- utils/SmsUtils.java
+++ utils/SmsUtils.java
@@ -32,8 +32,8 @@ public class SmsUtils {
         final String product = "Dysmsapi";//短信API产品名称（短信产品名固定，无需修改）
         final String domain = "dysmsapi.aliyuncs.com";//短信API产品域名（接口地址固定，无需修改）
         //替换成你的AK
-        final String accessKeyId = "LTAI4FfaoSn36Fjk4M9g6N8q";//你的accessKeyId,参考本文档步骤2
-        final String accessKeySecret = "nl80TKxVE4cetoV20UdzEdelY8JAGE";//你的accessKeySecret，参考本文档步骤2
+        final String accessKeyId = "xxxx";//你的accessKeyId,参考本文档步骤2
+        final String accessKeySecret = "xxxx";//你的accessKeySecret，参考本文档步骤2
         //初始化ascClient,暂时不支持多region（请勿修改）
         IClientProfile profile = DefaultProfile.getProfile("cn-hangzhou", accessKeyId,
                 accessKeySecret);
@@ -46,9 +46,9 @@ public class SmsUtils {
         //必填:待发送手机号。支持以逗号分隔的形式进行批量调用，批量上限为1000个手机号码,批量调用相对于单条调用及时性稍有延迟,验证码类型的短信推荐使用单条调用的方式；发送国际/港澳台消息时，接收号码格式为00+国际区号+号码，如“0085200000000”
         request.setPhoneNumbers(moblie);
         //必填:短信签名-可在短信控制台中找到
-        request.setSignName("airubbish");
+        request.setSignName("ai-rubbish");
         //必填:短信模板-可在短信控制台中找到，发送国际/港澳台消息时，请使用国际/港澳台短信模版
-        request.setTemplateCode("SMS_198677151");
+        request.setTemplateCode("SMS_198677134");
         //可选:模板中的变量替换JSON串,如模板内容为"亲爱的${name},您的验证码为${code}"时,此处的值为
         //友情提示:如果JSON中需要带换行符,请参照标准的JSON协议对换行符的要求,比如短信内容中包含\r\n的情况在JSON中需要表示成\\r\\n,否则会导致JSON在服务端解析失败
         request.setTemplateParam("{\"code\":\"" + i + "\"}");
